9868b560ac2dce0f7dd62765275a34db585f7084
Aline Soliman <alineapreis@hotmail.com>
2023-02-17 08:56:29 -0300

    cria funções para pegar os dados csv

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -7,3 +7,33 @@ def get_maria_order(maria_food, order):
         maria_food[food] = 1
     else:
         maria_food[food] += 1
+
+
+def data_from_csv(
+    maria_food,
+):
+    most_ordered = None
+    ordered_times = 0
+
+    for order in maria_food:
+        if maria_food[order] > ordered_times:
+            most_ordered = order
+            ordered_times = maria_food[order]
+
+    with open("data/mkt_campaign.txt", mode="w") as file:
+        file.writelines(f"{most_ordered}\n")
+        file.close()
+
+
+def get_csv_data(csv_list):
+    maria_food = {}
+
+    for client, order in csv_list:
+        if client == "maria":
+            get_maria_order(maria_food, order)
+
+    data_from_csv(maria_food)
+
+
+def analyze_log(path_to_file):
+    pass
